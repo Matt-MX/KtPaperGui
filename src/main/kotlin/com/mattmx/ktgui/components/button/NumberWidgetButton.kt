22c9166308c295b1bb4accaabc58acb0d91a0dde
MattMXX <mangrovethefox@outlook.com>
2022-08-30 00:17:39 +0100

    Added new example Button type

--- src/main/kotlin/com/mattmx/ktgui/components/button/NumberWidgetButton.kt
+++ src/main/kotlin/com/mattmx/ktgui/components/button/NumberWidgetButton.kt
@@ -1,4 +1,41 @@
 package com.mattmx.ktgui.components.button
 
-class NumberWidgetButton {
+import com.mattmx.ktgui.components.screen.IGuiScreen
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.ClickType
+import org.bukkit.event.inventory.InventoryClickEvent
+import java.lang.Integer.max
+import java.lang.Integer.min
+
+class NumberWidgetButton(
+    material: Material = Material.STONE,
+    val startValue: Int = 1,
+    val step: Int = 1
+) : GuiButton(material) {
+    init {
+        amount(startValue)
+    }
+
+    override fun thisClicked(e: InventoryClickEvent) {
+        when (e.click) {
+            ClickType.RIGHT -> {
+                amount(min(64, max(1, item!!.amount + step)))
+                update(e.whoClicked as Player)
+            }
+            ClickType.LEFT -> {
+                amount(min(64, max(1, item!!.amount - step)))
+                update(e.whoClicked as Player)
+            }
+            else -> {}
+        }
+    }
+
+    override fun copy(parent: IGuiScreen): NumberWidgetButton {
+        val n = NumberWidgetButton(startValue = this.startValue, step = this.step)
+        n.item = item
+        n.amount(this.startValue)
+        n.parent = parent
+        return n
+    }
 }
--- src/main/kotlin/com/mattmx/ktgui/examples/ConfigScreenExample.kt
+++ src/main/kotlin/com/mattmx/ktgui/examples/ConfigScreenExample.kt
@@ -1,6 +1,7 @@
 package com.mattmx.ktgui.examples
 
 import com.mattmx.ktgui.components.button.GuiToggleButton
+import com.mattmx.ktgui.components.button.NumberWidgetButton
 import com.mattmx.ktgui.components.screen.GuiScreen
 import com.mattmx.ktgui.item.ItemBuilder
 import com.mattmx.ktgui.utils.Chat
@@ -11,7 +12,7 @@ class ConfigScreenExample : GuiScreen("Example Config", 3) {
         val enabled = ItemBuilder(Material.LIME_STAINED_GLASS_PANE).name("&aEnabled")
         val disabled = ItemBuilder(Material.RED_STAINED_GLASS_PANE).name("&cDisabled")
         var slot = 10
-        repeat(4) {
+        repeat(3) {
             GuiToggleButton(enabled.copy().lore("&8This is item $it").make(),
                 disabled.copy().lore("&8This is item $it").make(),
                 false
@@ -20,5 +21,10 @@ class ConfigScreenExample : GuiScreen("Example Config", 3) {
             } childOf this slot slot
             slot += 2
         }
+        NumberWidgetButton()
+            .lore {
+                it.add("&8Right click to increase")
+                it.add("&8Left click to decrease")
+            } material Material.BLUE_STAINED_GLASS_PANE named "&9Amount widget" childOf this slot 16
     }
 }
