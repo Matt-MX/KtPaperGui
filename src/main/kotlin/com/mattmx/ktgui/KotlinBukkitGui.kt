1e20a26d4fdfdd05e9b9b87f712a8245a6e24842
MattMXX <mangrovethefox@outlook.com>
2022-08-29 17:32:06 +0100

    Update checker

--- src/main/kotlin/com/mattmx/ktgui/KotlinBukkitGui.kt
+++ src/main/kotlin/com/mattmx/ktgui/KotlinBukkitGui.kt
@@ -2,23 +2,32 @@ package com.mattmx.ktgui
 
 import com.mattmx.ktgui.commands.KtGuiCommand
 import com.mattmx.ktgui.examples.CustomGUI
+import com.mattmx.ktgui.utils.GitUpdateChecker
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.logging.Logger
 
 class KotlinBukkitGui : JavaPlugin() {
     override fun onEnable() {
-        // register example guis
         version = description.version
         log = this.logger
+        Bukkit.getScheduler().runTaskAsynchronously(this) { ->
+            GitUpdateChecker("https://api.github.com/repos/Matt-MX/KtBukkitGui/releases/latest", version,
+                { outdated, latest ->
+                    if (outdated) {
+                        log.info("Running an outdated version (v$version) Latest available (v$latest)")
+                        log.info("Download here: https://github.com/Matt-MX/KtBukkitGui/releases/latest")
+                    } else log.info("Running latest version! (v$version)")
+                }, { _ -> log.info("Unable to check for latest version.") })
+        }
         GuiManager.init(this)
         GuiManager.register("example_normal", CustomGUI())
         Bukkit.getPluginCommand("ktgui")?.setExecutor(KtGuiCommand())
     }
 
     companion object {
-        val papi : Boolean = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null
-        lateinit var version : String
-        lateinit var log : Logger
+        val papi: Boolean = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null
+        lateinit var version: String
+        lateinit var log: Logger
     }
 }
--- src/main/kotlin/com/mattmx/ktgui/utils/GitUpdateChecker.kt
+++ src/main/kotlin/com/mattmx/ktgui/utils/GitUpdateChecker.kt
@@ -1,17 +1,23 @@
 package com.mattmx.ktgui.utils
 
 import com.google.gson.JsonParser
+import java.lang.Exception
 import java.net.URL
 
 class GitUpdateChecker(
     val url: String,
     val current: String,
-    val then: ((Boolean, String) -> Unit)? = null
+    val then: ((Boolean, String) -> Unit)? = null,
+    val error: ((Exception) -> Unit)? = null
 ) {
     init {
-        val str = URL(url).readText()
-        val json = JsonParser.parseString(str).asJsonObject
-        val latestRelease = json.get("tag_name").asString
-        then?.invoke(current != latestRelease, latestRelease)
+        try {
+            val str = URL(url).readText()
+            val json = JsonParser.parseString(str).asJsonObject
+            val latestRelease = json.get("tag_name").asString
+            then?.invoke(current != latestRelease, latestRelease)
+        } catch (e: Exception) {
+            error?.invoke(e)
+        }
     }
 }
