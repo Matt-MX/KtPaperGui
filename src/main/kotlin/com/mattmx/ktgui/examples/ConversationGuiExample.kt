aa2680d0d23d5f3d2d41dc380c70c872d1fcaa8f
MattMXX <mangrovethefox@outlook.com>
2022-10-09 04:35:23 +0100

    Conversation API impl

--- src/main/kotlin/com/mattmx/ktgui/examples/ConversationGuiExample.kt
+++ src/main/kotlin/com/mattmx/ktgui/examples/ConversationGuiExample.kt
@@ -21,35 +21,37 @@ class ConversationGuiExample() : GuiScreen("Conversation API", 1) {
         GuiButton()
             .click {
                 left = { e ->
-                    forceClose(e.whoClicked as Player)
+                    val player = e.whoClicked as Player
+                    forceClose(player)
                     conversation(KotlinBukkitGui.plugin!!) {
                         stringPrompt(Chat.color("&6&lWhat's your fave fruit")) { c, i ->
                             if (i == "oranges") {
-                                c.forWhom.sendRawMessage(Chat.color("&eI love oranges"))
-                            } else c.forWhom.sendRawMessage(Chat.color("&eEwwww"))
+                                player.sendMessage(Chat.color("&eI love oranges"))
+                            } else player.sendMessage(Chat.color("&eEwwww"))
                         }
                         numberPrompt(Chat.color("&6&lHow many fruit do you eat a day?")) { c, i ->
-                            c.forWhom.sendRawMessage(Chat.color("&eYou inputted $i"))
+                            player.sendMessage(Chat.color("&eYou inputted $i"))
                         }
                         finish(Chat.color("&cNice talking to you!")) {
-                            open(e.whoClicked as Player)
+                            open(player)
                         }
                     }.abandon {
-                        open(e.whoClicked as Player)
-                        (it.context.forWhom as Player).sendMessage(Chat.color("&cExited Conversation"))
+                        open(player)
+                        player.sendMessage(Chat.color("&cExited Conversation"))
                     }.exitOn("exit")
-                        .build(e.whoClicked as Player).begin()
+                        .build(player).begin()
                 }
                 right = { e ->
-                    forceClose(e.whoClicked as Player)
+                    val player = e.whoClicked as Player
+                    forceClose(player)
                     conversation(KotlinBukkitGui.plugin!!) {
                         stringPrompt(Chat.color("&6&lEnter a GUI name")) { c, i ->
                             i?.let { title(i) }
                         }
                         abandon {
-                            openAndFormat(e.whoClicked as Player)
+                            openAndFormat(player)
                         }
-                    }.build(e.whoClicked as Player).begin()
+                    }.build(player).begin()
                 }
             } named "&6&lChange GUI Name" childOf this slot 4 material Material.NAME_TAG
     }
