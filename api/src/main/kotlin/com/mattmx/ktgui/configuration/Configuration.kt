8d3e4ce345000feb30752fdf732af219d425ddb5
MattMXX <mangrovethefox@outlook.com>
2023-10-26 02:17:12 +0100

    Adding per-plugin configuration
    This is just an idea I had, I might completely remove it, but I think having access to underlying, previously hard-coded messages is a good idea.

---
+++ api/src/main/kotlin/com/mattmx/ktgui/configuration/Configuration.kt
@@ -0,0 +1,17 @@
+package com.mattmx.ktgui.configuration
+
+data class Configuration(
+    var commandConfiguration: CommandConfiguration = CommandConfiguration()
+) {
+    fun commands(block: CommandConfiguration.() -> Unit) {
+        val default = CommandConfiguration()
+        block(default)
+        this.commandConfiguration = default
+    }
+}
+
+data class CommandConfiguration(
+    var unknownCommandMessage: String = "&cUnknown command.",
+    var invalidPermissionsMessage: String = "&cYou do not have permissions to do this.",
+    var playerOnlyCommandMessage: String = "&cThis is a player-only command!"
+)
