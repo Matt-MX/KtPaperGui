eb332c4c7d3754dd4c5cfd27341160c7b6014201
MattMXX <mangrovethefox@outlook.com>
2023-10-26 02:16:36 +0100

    GuiPattern docs

--- api/src/main/kotlin/com/mattmx/ktgui/components/GuiPattern.kt
+++ api/src/main/kotlin/com/mattmx/ktgui/components/GuiPattern.kt
@@ -12,6 +12,11 @@ class GuiPattern(
     private val items = hashMapOf<Char, IGuiButton>()
     var blankSpaceChar = '-'
 
+    /**
+     * Applies the generated gui pattern to a gui screen.
+     *
+     * @param guiScreen the gui we want to modify.
+     */
     fun apply(guiScreen: GuiScreen) {
         // Can't effectively apply the Gui Pattern to a non-row gui
         if (guiScreen.type != null) return
@@ -21,6 +26,13 @@ class GuiPattern(
         }
     }
 
+    /**
+     * Mostly internal method to create a map of the gui buttons
+     * we want to create.
+     *
+     * @param rows the number of rows the gui will have.
+     * @return a compiled map of Buttons to their respective slots in the gui.
+     */
     fun build(rows: Int) : Map<IGuiButton, ArrayList<Int>> {
         val map = mutableMapOf<IGuiButton, ArrayList<Int>>()
         val maxSlot = rows * 9
@@ -45,16 +57,41 @@ class GuiPattern(
 
     fun getPattern() = pattern
 
-    fun trimPattern() {
+    private fun trimPattern() {
         pattern = pattern.replace("\n", "")
             .replace("\r\n", "")
     }
 
+    /**
+     * Sets a slot to a specified [ItemStack].
+     *
+     * @param char target character specified in the [pattern]
+     * @param item that the char will be set to
+     */
     operator fun set(char: Char, item: ItemStack) = items.put(char, GuiButton(item = item))
+
+    /**
+     * Sets a slot to a specified [IGuiButton].
+     *
+     * @param char target character specified in the [pattern]
+     * @param button that the char will be set to
+     */
     operator fun set(char: Char, button: IGuiButton) = items.put(char, button)
+
+    /**
+     * Returns the gui button of a character
+     *
+     * @param char
+     * @return gui button of a character, or null if it hasn't been set yet
+     */
     operator fun get(char: Char) : IGuiButton? = items[char]
 }
 
+/**
+ * Extension method to apply the gui pattern in the context of a [GuiScreen]
+ *
+ * @param pattern the pattern to apply
+ */
 fun GuiScreen.applyPattern(pattern: GuiPattern) {
     pattern.apply(this)
 }
