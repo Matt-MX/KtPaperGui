c046c0fc26ff81cf08461f3301bb4c7a497c2219
MattMXX <mangrovethefox@outlook.com>
2023-11-18 21:25:12 +0000

    Update tests.kt

--- api/src/main/kotlin/com/mattmx/ktgui/commands/smart/tests.kt
+++ api/src/main/kotlin/com/mattmx/ktgui/commands/smart/tests.kt
@@ -2,8 +2,11 @@ package com.mattmx.ktgui.commands.smart
 
 import kotlin.concurrent.thread
 
-fun <S : CommandSender, T> KtCommandBuilder<S>.argument(suggests: (CommandContext<S>.() -> List<String>)?) =
-    Argument<S, T, String>(ArgumentType.REQUIRED_SINGLE, { c -> c.args[0] }, suggests).apply { this@argument.expectedArguments += this }
+fun <S : CommandSender, T> KtCommandBuilder<S>.argument(description: String? = null, suggests: (CommandContext<S>.() -> List<String>)? = null) =
+    Argument<S, T, String>(ArgumentType.REQUIRED_SINGLE, { c -> c.args[0] }, suggests).apply {
+        this withDescription description
+        this@argument.expectedArguments += this
+    }
 
 fun <S : CommandSender> command(name: String, builder: KtCommandBuilder<S>.() -> Unit) =
     KtCommandBuilder<S>(name).apply(builder)
@@ -11,7 +14,10 @@ fun <S : CommandSender> command(name: String, builder: KtCommandBuilder<S>.() ->
 fun argsOf(string: String) = string.split(" ").toTypedArray()
 fun main() {
     val msg = command<CommandSender>("msg") {
-        val username by argument { listOf("meow") }
+        val username by argument("The user of whom to directly message.") { listOf("meow") }
+        val message by argument("The message you would like to send.")
+
+        permission { /* Boolean Condition */ true }
 
         runs {
             thread {
@@ -21,7 +27,7 @@ fun main() {
         }
     }
 
-    println(msg.getUsage())
+    println(msg.getUsage(true))
     msg(CommandContext(CommandSender(), argsOf("MattMX")))
     msg(CommandContext(CommandSender(), argsOf("GabbySimon")))
 }
