54e57cc3700b0260b938895b15a8a365a9a8f94e
MattMXX <mangrovethefox@outlook.com>
2023-11-20 15:36:07 +0000

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
 rootProject.name = "ktgui"
 include("api")
 include("plugin")
+
+plugins {
+    id("com.gradle.enterprise") version("3.15")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
