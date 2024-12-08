bd5e415b9e6d1fb04161195324ead90052151b00
MattMX <mangrovethefox@outlook.com>
2024-12-08 14:02:04 +0000

    :fire: Add specific gui types

--- ktgui-core/src/main/kotlin/com/mattmx/ktgui/screen/GuiType.kt
+++ ktgui-core/src/main/kotlin/com/mattmx/ktgui/screen/GuiType.kt
@@ -41,6 +41,25 @@ object InventoryTypes {
     val GENERIC_9x4 = InventoryType(3, 9 * 4)
     val GENERIC_9x5 = InventoryType(4, 9 * 5)
     val GENERIC_9x6 = InventoryType(5, 9 * 6)
+
+    val ANVIL = InventoryType(8, 3)
+    val BEACON = InventoryType(9, -1)
+    val BLAST_FURNACE = InventoryType(10, -1)
+    val BREWING_STAND = InventoryType(11, -1)
+    val CRAFTING_TABLE = InventoryType(12, -1)
+    val ENCHANTMENT_TABLE = InventoryType(13, -1)
+    val FURNACE = InventoryType(14, -1)
+    val GRINDSTONE = InventoryType(15, -1)
+    val HOPPER = InventoryType(16, -1)
+    val LECTERN = InventoryType(17, -1)
+    val LOOM = InventoryType(18, -1)
+    val VILLAGER = InventoryType(19, -1)
+    val SHULKER_BOX = InventoryType(20, -1)
+    val SMITHING_TABLE = InventoryType(21, -1)
+    val SMOKER = InventoryType(22, -1)
+    val CARTOGRAPHY_TABLE = InventoryType(23, -1)
+    val STONECUTTER = InventoryType(24, -1)
+
     val GENERICS = arrayOf(
         GENERIC_9x1,
         GENERIC_9x2,
