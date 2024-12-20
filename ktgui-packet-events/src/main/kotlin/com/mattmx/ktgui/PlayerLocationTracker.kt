48340d07e7fd44935ea04aa8a64c5d673ef4b394
MattMX <mangrovethefox@outlook.com>
2024-12-20 16:41:29 +0000

    :fire: Track player locations

---
+++ ktgui-packet-events/src/main/kotlin/com/mattmx/ktgui/PlayerLocationTracker.kt
@@ -0,0 +1,41 @@
+package com.mattmx.ktgui
+
+import com.github.retrooper.packetevents.event.PacketListenerAbstract
+import com.github.retrooper.packetevents.event.PacketSendEvent
+import com.github.retrooper.packetevents.protocol.packettype.PacketType
+import com.github.retrooper.packetevents.util.Vector3d
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerPositionAndLook
+import java.util.*
+
+class PlayerLocationTracker : PacketListenerAbstract() {
+    private val locations = Collections.synchronizedMap(hashMapOf<Any, CachedLocation>())
+
+    override fun onPacketSend(event: PacketSendEvent) {
+        val player = event.getPlayer<Any>()
+        when (event.packetType) {
+            PacketType.Play.Server.PLAYER_POSITION_AND_LOOK -> {
+                val packet = WrapperPlayServerPlayerPositionAndLook(event)
+
+                val cache = locations.getOrPut(player) { CachedLocation() }
+                cache.pos = packet.position
+                cache.yaw = packet.yaw
+                cache.pitch = packet.pitch
+            }
+        }
+    }
+
+    fun remove(player: Any): CachedLocation? {
+        return locations.remove(player)
+    }
+
+    fun getCachedLocation(player: Any): CachedLocation? {
+        return locations[player]
+    }
+
+    class CachedLocation(
+        var pos: Vector3d = Vector3d.zero(),
+        var yaw: Float = 0f,
+        var pitch: Float = 0f
+    )
+
+}
